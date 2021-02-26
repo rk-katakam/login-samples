93a9cd45e27fc269525d209704a511fb8a41aa33
Rajanikanth Katakam <36837542+rk-katakam@users.noreply.github.com>
2021-02-26 17:51:40 +0530

    Add files via upload
    Skew View

---
+++ login/login.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent
+  }
+  
+];
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
+  ]
+})
+export class LoginModule { }
